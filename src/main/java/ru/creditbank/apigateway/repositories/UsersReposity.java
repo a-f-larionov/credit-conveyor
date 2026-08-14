1d60f4afe0642651eefe4192d41a1006135cd152
a.f.larionov <a.f.larionov@gmail.com>
2026-08-14 15:57:12 +0300

    add user repository, postgresql driver, data jpa and migrations

---
+++ src/main/java/ru/creditbank/apigateway/repositories/UsersReposity.java
@@ -0,0 +1,17 @@
+package ru.creditbank.apigateway.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import ru.creditbank.apigateway.core.UserModel;
+
+import java.util.Optional;
+
+@Repository
+public interface UsersReposity extends PagingAndSortingRepository<UserModel, Long> {
+
+    boolean existsByEmail(String email);
+
+    void save(UserModel user);
+
+    Optional<UserModel> findByEmail(String email);
+}
